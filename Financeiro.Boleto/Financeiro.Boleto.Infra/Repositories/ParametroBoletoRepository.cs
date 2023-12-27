cf5ca5768414898f6950d82599e9a5f448e1e860
Felipe Junges <felipejunges@yahoo.com.br>
2023-12-27 12:40:34 -0300

    ✨ Criada rota para incrementar NumeroBoleto na API boleto

--- Financeiro.Boleto/Financeiro.Boleto.Infra/Repositories/ParametroBoletoRepository.cs
+++ Financeiro.Boleto/Financeiro.Boleto.Infra/Repositories/ParametroBoletoRepository.cs
@@ -17,42 +17,55 @@ namespace Financeiro.Boleto.Infra.Repositories
             _context = context;
         }
 
-        public async Task<ParametroBoleto?> ObterParametrosBoleto()
+        public async Task<ParametroBoleto> ObterParametrosBoleto()
         {
-            await TEMP();
-
-            return await _context.ParametrosBoleto
+            var parametros = await _context.ParametrosBoleto
                             .Where(p => p.Id == PARAMETRO_BOLETO_PADRAO)
                             .FirstOrDefaultAsync();
+
+            if (parametros is not null)
+                return parametros;
+
+            return await CriarParametrosBoleto();
         }
 
-        private async Task TEMP()
+        public async Task<int> IncrementarNumeroBoletoAtual()
         {
-            if (await _context.ParametrosBoleto.AnyAsync())
-                return;
+            var parametros = await ObterParametrosBoleto();
 
-            await _context.ParametrosBoleto.AddAsync(
-                new ParametroBoleto(
-                        id: 1,
-                        descricao: "Boleto Bradesco",
-                        banco: "237",
-                        agencia: "1234-5",
-                        numeroConta: "123456-0",
-                        carteira: "12",
-                        numeroBoletoAtual: 0,
-                        nomeBeneficiario: "Financeiro Aula Solutions",
-                        cnpjBeneficiario: "09.934.582/0001-58",
-                        enderecoBeneficiario: new Endereco(
-                            cep: "93000-000",
-                            logradouro: "Rua das Empresas",
-                            numero: "112",
-                            complemento: "",
-                            bairro: "Centro",
-                            municipio: "Porto Alegre",
-                            uf: "RS")
-                        ));
+            parametros.IncrementarNumeroBoletoAtual();
 
             await _context.SaveChangesAsync();
+
+            return parametros.NumeroBoletoAtual;
+        }
+
+        private async Task<ParametroBoleto> CriarParametrosBoleto()
+        {
+            var parametros = new ParametroBoleto(
+                                    id: PARAMETRO_BOLETO_PADRAO,
+                                    descricao: "Boleto Bradesco",
+                                    banco: "237",
+                                    agencia: "1234-5",
+                                    numeroConta: "123456-0",
+                                    carteira: "12",
+                                    numeroBoletoAtual: 0,
+                                    nomeBeneficiario: "Financeiro Aula Solutions",
+                                    cnpjBeneficiario: "09.934.582/0001-58",
+                                    enderecoBeneficiario: new Endereco(
+                                        cep: "93000-000",
+                                        logradouro: "Rua das Empresas",
+                                        numero: "112",
+                                        complemento: "",
+                                        bairro: "Centro",
+                                        municipio: "Porto Alegre",
+                                        uf: "RS")
+                                    );
+
+            await _context.ParametrosBoleto.AddAsync(parametros);
+            await _context.SaveChangesAsync();
+
+            return parametros;
         }
     }
 }
